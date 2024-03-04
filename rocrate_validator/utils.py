6d4661792723e3a9c2ec299a97f8b6b4f96002a1
Marco Enrico Piras <kikkomep@crs4.it>
2024-03-04 16:52:07 +0100

    refactor(utils): :recycle: generic function to load graphs from paths

--- rocrate_validator/utils.py
+++ rocrate_validator/utils.py
@@ -1,4 +1,15 @@
+import logging
 import os
+from typing import List
+
+from rdflib import Graph
+
+# current directory
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+# set up logging
+logger = logging.getLogger(__name__)
 
 
 def get_all_files(directory: str = '.', extension: str = '.ttl'):
@@ -14,3 +25,22 @@ def get_all_files(directory: str = '.', extension: str = '.ttl'):
                 file_paths.append(os.path.join(root, file))
     # return the list of file paths
     return file_paths
+
+
+def get_graphs_paths(graphs_dir: str = CURRENT_DIR) -> List[str]:
+    """
+    Get all the SHACL shapes files in the shapes directory
+    """
+    return get_all_files(directory=graphs_dir, extension='.ttl')
+
+
+def get_full_graph(graphs_dir: str, publicID: str = ".") -> Graph:
+    """
+    Get the SHACL shapes graph
+    """
+    full_graph = Graph()
+    graphs_paths = get_graphs_paths(graphs_dir)
+    for graph_path in graphs_paths:
+        full_graph.parse(graph_path, format="turtle", publicID=publicID)
+        logger.debug("Loaded triples from %s", graph_path)
+    return full_graph
