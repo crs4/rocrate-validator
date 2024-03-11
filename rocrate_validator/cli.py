12faaa94166cf4152a654dde142c86b71710f885
Marco Enrico Piras <kikkomep@crs4.it>
2024-03-11 14:46:45 +0100

    feat(cli): :sparkles: add command to list all available profiles

--- rocrate_validator/cli.py
+++ rocrate_validator/cli.py
@@ -3,10 +3,13 @@ import os
 
 import rich_click as click
 from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+from rich.markdown import Markdown
 
+from . import services
 from .colors import get_severity_color
 from .models import Severity, ValidationResult
-from .services import validate as validate_rocrate
 
 # set up logging
 logger = logging.getLogger(__name__)
@@ -35,6 +38,66 @@ def cli(ctx, debug: bool = False):
     if ctx.invoked_subcommand is None:
         # If no subcommand is provided, invoke the default command
         ctx.invoke(validate)
+
+
+@cli.group("profiles")
+def profiles():
+    """
+    [magenta]rocrate-validator:[/magenta] Manage profiles
+    """
+    pass
+
+
+@profiles.command("list")
+@click.option(
+    "-p",
+    "--profiles-path",
+    type=click.Path(exists=True),
+    default="./profiles",
+    show_default=True,
+    help="Path containing the profiles files",
+)
+@click.pass_context
+def list_profiles(ctx, profiles_path: str = "./profiles"):
+    """
+    List available profiles
+    """
+    profiles = services.get_profiles(profiles_path=profiles_path)
+    # console.print("\nAvailable profiles:", style="white bold")
+    console.print("\n", style="white bold")
+
+    table = Table(show_header=True,
+                  title="Available profiles",
+                  header_style="bold cyan",
+                  border_style="bright_black",
+                  show_footer=True,
+                  caption="(*) Number of requirements by severity")
+
+    # Define columns
+    table.add_column("Name", style="magenta bold")
+    table.add_column("Description", style="white italic")
+    table.add_column("Requirements (*)", style="white")
+
+    # Add data to the table
+    for profile_name, profile in profiles.items():
+        # Count requirements by severity
+        requirements = {}
+        logger.debug("Requirements: %s", requirements)
+        for req in profile.requirements:
+            if not requirements.get(req.type.name, None):
+                requirements[req.type.name] = 0
+            requirements[req.type.name] += 1
+        requirements = ", ".join(
+            [f"[bold][{get_severity_color(severity)}]{severity}: "
+             f"{count}[/{get_severity_color(severity)}][/bold]"
+             for severity, count in requirements.items() if count > 0])
+
+        # Add the row to the table
+        table.add_row(profile_name, Markdown(profile.description.strip()), requirements)
+        table.add_row()
+
+    # Print the table
+    console.print(table)
 
 
 @cli.command("validate")
@@ -125,7 +188,7 @@ def validate(ctx,
     try:
 
         # Validate the RO-Crate
-        result: ValidationResult = validate_rocrate(
+        result: ValidationResult = services.validate(
             profiles_path=profiles_path,
             profile_name=profile_name,
             requirement_level=requirement_level,
