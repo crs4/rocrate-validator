d709c91386ebc34559d4950eed3ca277b37e74d2
Marco Enrico Piras <kikkomep@crs4.it>
2024-03-11 14:44:09 +0100

    feat(utils): :sparkles: color by severity name

--- rocrate_validator/colors.py
+++ rocrate_validator/colors.py
@@ -1,28 +1,30 @@
+from typing import Union
+
 from .models import Severity
 
 
-def get_severity_color(severity: Severity) -> str:
+def get_severity_color(severity: Union[str, Severity]) -> str:
     """
     Get the color for the severity
 
     :param severity: The severity
     :return: The color
     """
-    if severity == Severity.ERROR:
+    if severity == Severity.ERROR or severity == "ERROR":
         return "red"
-    elif severity == Severity.MUST:
+    elif severity == Severity.MUST or severity == "MUST":
         return "red"
-    elif severity == Severity.MUST_NOT:
+    elif severity == Severity.MUST_NOT or severity == "MUST_NOT":
         return "purple"
-    elif severity == Severity.SHOULD:
+    elif severity == Severity.SHOULD or severity == "SHOULD":
         return "yellow"
-    elif severity == Severity.SHOULD_NOT:
+    elif severity == Severity.SHOULD_NOT or severity == "SHOULD_NOT":
         return "lightyellow"
-    elif severity == Severity.MAY:
+    elif severity == Severity.MAY or severity == "MAY":
         return "orange"
-    elif severity == Severity.INFO:
+    elif severity == Severity.INFO or severity == "INFO":
         return "lightblue"
-    elif severity == Severity.WARNING:
+    elif severity == Severity.WARNING or severity == "WARNING":
         return "yellow green"
     else:
         return "white"
