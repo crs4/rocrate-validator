8e0485287abc4108bf632bdfff1676afb9dbb92a
Marco Enrico Piras <kikkomep@crs4.it>
2024-03-11 19:09:39 +0100

    fix(core): :pencil2: typo

--- rocrate_validator/models.py
+++ rocrate_validator/models.py
@@ -67,7 +67,7 @@ class RequirementType:
 
 class RequirementLevels:
     """
-    * The key words MUST, MUST NOT, REQUIRED,
+    * The keywords MUST, MUST NOT, REQUIRED,
     * SHALL, SHALL NOT, SHOULD, SHOULD NOT,
     * RECOMMENDED, MAY, and OPTIONAL in this document
     * are to be interpreted as described in RFC 2119.
