ababe67d2b7eb77713d35a71ac627d0764b3c75f
Marco Enrico Piras <kikkomep@crs4.it>
2024-03-24 16:19:11 +0100

    test(profiles/ro-crate): :white_check_mark: add test for @type requirements of JSON-LD entities

--- tests/integration/profiles/ro-crate/must/test_file_descriptor.py
+++ tests/integration/profiles/ro-crate/must/test_file_descriptor.py
@@ -134,3 +134,28 @@ def test_not_valid_jsonld_format_missing_ids(paths):
         logger.debug(f"Detected issue {type(issue)}: {issue.message}")
 
 
+def test_not_valid_jsonld_format_missing_types(paths):
+
+    rocrate_path = f"{paths.invalid_jsonld_format}/missing_type"
+    logger.debug(f"test_missing_file_descriptor: {rocrate_path}")
+
+    result: models.ValidationResult = services.validate(rocrate_path,
+                                                        requirement_level="MUST", abort_on_first=True)
+    assert not result.passed(), "ro-crate should be invalid"
+
+    # check requirement
+    failed_requirements = result.failed_requirements
+    for failed_requirement in failed_requirements:
+        logger.debug(f"Failed requirement: {failed_requirement}")
+    assert len(failed_requirements) == 1, "ro-crate should have 1 failed requirement"
+
+    # set reference to failed requirement
+    failed_requirement = failed_requirements[0]
+    logger.debug(f"Failed requirement name: {failed_requirement.name}")
+
+    # check if the failed requirement is the expected one
+    assert failed_requirement.name == "FileDescriptorJsonLdFormat", \
+        "Unexpected failed requirement"
+
+    for issue in result.get_issues():
+        logger.debug(f"Detected issue {type(issue)}: {issue.message}")
