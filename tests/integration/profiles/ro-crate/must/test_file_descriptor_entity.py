54e17ed5653b04c92e4de169d5611261791d05fd
simleo <simleo@crs4.it>
2024-04-22 16:31:13 +0200

    constrain the descriptor's about to be the root

--- tests/integration/profiles/ro-crate/must/test_file_descriptor_entity.py
+++ tests/integration/profiles/ro-crate/must/test_file_descriptor_entity.py
@@ -45,6 +45,17 @@ def test_missing_entity_about():
     )
 
 
+def test_invalid_entity_about():
+    """Test a RO-Crate with an invalid about property in the file descriptor."""
+    do_entity_test(
+        paths.invalid_entity_about,
+        models.Severity.REQUIRED,
+        False,
+        ["RO-Crate Metadata File Descriptor: recommended properties"],
+        ["The RO-Crate metadata file descriptor MUST have an `about` property referencing the Root Data Entity"]
+    )
+
+
 def test_invalid_entity_about_type():
     """Test a RO-Crate with an invalid file descriptor entity type."""
     do_entity_test(
