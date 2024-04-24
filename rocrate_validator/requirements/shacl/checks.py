5d084ca9adbaed0d99d5e77c6aacc3d313db483e
Marco Enrico Piras <kikkomep@crs4.it>
2024-04-24 15:39:38 +0200

    fix(shacl): :construction: workaround to resolve ex: prefix on sh:select strings

--- rocrate_validator/requirements/shacl/checks.py
+++ rocrate_validator/requirements/shacl/checks.py
@@ -1,6 +1,10 @@
 import logging
+import os
 from typing import Optional
 
+from rdflib import Literal, Namespace
+
+from rocrate_validator.constants import SHACL_NS
 from rocrate_validator.models import (Requirement, RequirementCheck,
                                       ValidationContext)
 from rocrate_validator.requirements.shacl.models import Shape
@@ -34,6 +38,17 @@ class SHACLCheck(RequirementCheck):
         ontology_graph = context.validator.ontologies_graph
         data_graph = context.validator.data_graph
         shapes_graph = self.shape.graph
+
+        # temporary fix to replace the ex: prefix with the rocrate path
+        if os.path.isdir(context.validator.rocrate_path):
+            shacl_ns = Namespace(SHACL_NS)
+            selects = shapes_graph.triples((None, shacl_ns.select, None))
+            for s, p, o in selects:
+                shapes_graph.remove((s, p, o))
+                # FIXME: write a better regex ??
+                updated_node_value = str(o).replace("ex:", f"<file://{context.validator.rocrate_path}/>")
+                shapes_graph.add((s, p, Literal(updated_node_value)))
+
         # validate the data graph
         shacl_validator = SHACLValidator(shapes_graph=shapes_graph, ont_graph=ontology_graph)
         result = shacl_validator.validate(
