3ab6c35dfd73ba25dd31ea645d04a7a071ee293e
Marco Enrico Piras <kikkomep@crs4.it>
2024-09-16 10:11:57 +0200

    fix(shacl): :ambulance: report a generic error when the metadata is invalid

--- rocrate_validator/requirements/shacl/checks.py
+++ rocrate_validator/requirements/shacl/checks.py
@@ -132,8 +132,11 @@ class SHACLCheck(RequirementCheck):
             end_time = timer()
             logger.debug(f"Execution time for getting data graph: {end_time - start_time} seconds")
         except json.decoder.JSONDecodeError as e:
-            logger.debug("Unable to perform metadata validation due to an error in the JSON-LD data file: %s", e)
-            return False
+            logger.debug("Unable to perform metadata validation due to one or more errors in the JSON-LD data file: %s", e)
+            shacl_context.result.add_error(
+                "Unable to perform metadata validation due to one or more errors in the JSON-LD data file", self)
+            raise ROCrateMetadataNotFoundError(
+                "Unable to perform metadata validation due to one or more errors in the JSON-LD data file")
 
         # Begin the timer
         start_time = timer()
