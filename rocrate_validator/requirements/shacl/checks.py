1dc97fb0b565a1b0f0dd40ce3be19d2b9b896993
Marco Enrico Piras <kikkomep@crs4.it>
2024-09-16 10:20:16 +0200

    fix(core): :rotating_light: fix flake8 warning

--- rocrate_validator/requirements/shacl/checks.py
+++ rocrate_validator/requirements/shacl/checks.py
@@ -132,7 +132,8 @@ class SHACLCheck(RequirementCheck):
             end_time = timer()
             logger.debug(f"Execution time for getting data graph: {end_time - start_time} seconds")
         except json.decoder.JSONDecodeError as e:
-            logger.debug("Unable to perform metadata validation due to one or more errors in the JSON-LD data file: %s", e)
+            logger.debug("Unable to perform metadata validation "
+                         "due to one or more errors in the JSON-LD data file: %s", e)
             shacl_context.result.add_error(
                 "Unable to perform metadata validation due to one or more errors in the JSON-LD data file", self)
             raise ROCrateMetadataNotFoundError(
