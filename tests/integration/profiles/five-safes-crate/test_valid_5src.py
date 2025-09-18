cf2b8088f41a09ff82a5d036d6f0332ef40173ae
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-09-18 13:54:14 +0100

    tests for 5-safe crate request and result profiles

---
+++ tests/integration/profiles/five-safes-crate/test_valid_5src.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2024-2025 CRS4
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+from rocrate_validator.models import Severity
+from tests.conftest import SKIP_LOCAL_DATA_ENTITY_EXISTENCE_CHECK_IDENTIFIER
+from tests.ro_crates import ValidROC
+from tests.shared import do_entity_test
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def test_valid_five_safes_crate_request_required():
+    """Test a valid Workflow Run Crate."""
+    do_entity_test(
+        ValidROC().five_safes_crate_request,
+        Severity.REQUIRED,
+        True,
+        profile_identifier="five-safes-crate",
+        skip_checks=[SKIP_LOCAL_DATA_ENTITY_EXISTENCE_CHECK_IDENTIFIER]
+    )
+
+def test_valid_five_safes_crate_result_required():
+    """Test a valid Workflow Run Crate."""
+    do_entity_test(
+        ValidROC().five_safes_crate_result,
+        Severity.REQUIRED,
+        True,
+        profile_identifier="five-safes-crate",
+        skip_checks=[SKIP_LOCAL_DATA_ENTITY_EXISTENCE_CHECK_IDENTIFIER]
+    )
