a62b8a127d16f26ca9405a61250fdf39393144e9
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-10-21 12:07:24 +0100

    5safe root data entity tests use base crate now

--- tests/integration/profiles/five-safes-crate/test_5src_root_data_entity_metadata.py
+++ tests/integration/profiles/five-safes-crate/test_5src_root_data_entity_metadata.py
@@ -13,9 +13,10 @@
 # limitations under the License.
 
 import logging
+import rdflib
 
 from rocrate_validator.models import Severity
-from tests.ro_crates import Invalid5sROC
+from tests.ro_crates import ValidROC
 from tests.shared import do_entity_test
 
 # set up logging
@@ -26,8 +27,21 @@ def test_5src_root_data_entity_no_source_organization():
     """\
     Test a Five Safes Crate where the Root Data Entity it does not reference a sourceOrganization.
     """
+    def remove_source_org(graph):
+        SCHEMA = rdflib.Namespace("http://schema.org/")
+        target_subject = rdflib.URIRef("./")
+        target_predicate = SCHEMA.sourceOrganization
+        target_object = None
+
+        for s, p, o in graph.triples((target_subject, target_predicate, target_object)):
+            print(f"Removing: {s}, {p}, {o}")
+
+        graph.remove((target_subject, target_predicate, target_object))
+
+        return graph
+
     do_entity_test(
-        rocrate_path=Invalid5sROC().root_data_entity_no_source_organization,
+        rocrate_path=ValidROC().five_safes_crate_result,
         requirement_severity=Severity.REQUIRED,
         expected_validation_result=False,
         expected_triggered_requirements=["Five Safes Crate Root Data Entity REQUIRED properties"],
@@ -36,6 +50,7 @@ def test_5src_root_data_entity_no_source_organization():
                    SHOULD link to a Contextual Entity in the RO-Crate Metadata File with a name."""
         ],
         profile_identifier="five-safes-crate",
+        rocrate_entity_mod_function=remove_source_org,
     )
 
 
@@ -43,8 +58,23 @@ def test_5src_root_data_entity_source_organization_not_entity():
     """\
     Test a Five Safes Crate where the Root Data Entity it does not reference a sourceOrganization.
     """
+    def replace_source_org(graph):
+        SCHEMA = rdflib.Namespace("http://schema.org/")
+        target_subject = rdflib.URIRef("./")
+        target_predicate = SCHEMA.sourceOrganization
+        original_object = None
+        new_object = rdflib.Literal('Investigation of cancer (TRE72 project 81)')
+
+        for s, p, o in graph.triples((target_subject, target_predicate, original_object)):
+            print(f"Replacing: {s}, {p}, {o}")
+
+        graph.remove((target_subject, target_predicate, original_object))
+        graph.add((target_subject, target_predicate, new_object))
+
+        return graph
+
     do_entity_test(
-        rocrate_path=Invalid5sROC().root_data_entity_source_organization_not_entity,
+        rocrate_path=ValidROC().five_safes_crate_result,
         requirement_severity=Severity.REQUIRED,
         expected_validation_result=False,
         expected_triggered_requirements=["Five Safes Crate Root Data Entity REQUIRED properties"],
@@ -53,4 +83,5 @@ def test_5src_root_data_entity_source_organization_not_entity():
                    SHOULD link to a Contextual Entity in the RO-Crate Metadata File with a name."""
         ],
         profile_identifier="five-safes-crate",
+        rocrate_entity_mod_function=replace_source_org,
     )
