a12304594398b1fcc49b6170423e96165ee74797
Ettore Murabito <ettore.murabito@manchester.ac.uk>
2025-10-28 09:53:43 +0000

    Made line long shorter

--- tests/integration/profiles/five-safes-crate/test_5src_1_requesting_agent.py
+++ tests/integration/profiles/five-safes-crate/test_5src_1_requesting_agent.py
@@ -95,7 +95,7 @@ def test_5src_agent_memberOf_project_not_in_root():
         expected_validation_result=False,
         expected_triggered_requirements=["Agent Project Intersection"],
         expected_triggered_issues=[
-            """At least one Project referenced by Agent -> memberOf MUST be included in the set of Projects referenced by RootDataEntity -> sourceOrganization."""
+            """Projects referenced by Agent -> memberOf MUST intersect Projects referenced by RootDataEntity -> sourceOrganization."""
         ],
         profile_identifier="five-safes-crate",
         rocrate_entity_mod_sparql=sparql,
