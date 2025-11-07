11e017650bd9d47a305b2d49cec4470d12146994
Eli Chadwick <eli.chadwick.256@gmail.com>
2025-11-07 15:50:39 +0100

    mark valid test as xfail until bug is resolved

--- tests/integration/profiles/isa-ro-crate/test_valid_isa_crate.py
+++ tests/integration/profiles/isa-ro-crate/test_valid_isa_crate.py
@@ -18,11 +18,16 @@ from rocrate_validator.models import Severity
 from tests.conftest import SKIP_LOCAL_DATA_ENTITY_EXISTENCE_CHECK_IDENTIFIER
 from tests.ro_crates import ValidROC
 from tests.shared import do_entity_test
+import pytest
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+@pytest.mark.xfail(
+    reason="'File Descriptor JSON-LD format' check fails: The 17 occurrences of the JSON-LD key 'columnIndex' are not "
+    "allowed in the compacted format because it is not present in the @context of the document"
+)
 def test_valid_isa_ro_crate():
     """Test a valid ISA RO-Crate."""
     do_entity_test(
