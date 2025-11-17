6638185ed80118bf26394dee510b617d5b5f927f
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2025-11-17 11:58:13 +0000

    add check for signoff agent

--- tests/integration/profiles/five-safes-crate/test_5src_signoff_phase.py
+++ tests/integration/profiles/five-safes-crate/test_5src_signoff_phase.py
@@ -144,3 +144,34 @@ def test_5src_signoff_phase_no_actionstatus():
         profile_identifier="five-safes-crate",
         rocrate_entity_mod_sparql=sparql,
     )
+
+
+def test_5src_signoff_phase_no_agent():
+    """
+    Test a Five Safes Crate where the Sign-Off phase has no agent.
+    """
+    sparql = (
+        SPARQL_PREFIXES
+        + """
+        DELETE {
+            ?signoff schema:agent ?agent .
+        }
+        WHERE {
+           ?signoff a schema:AssessAction ;
+               schema:additionalType <https://w3id.org/shp#SignOff> ;
+               schema:agent ?agent .
+        }
+        """
+    )
+
+    do_entity_test(
+        rocrate_path=ValidROC().five_safes_crate_result,
+        requirement_severity=Severity.RECOMMENDED,
+        expected_validation_result=False,
+        expected_triggered_requirements=["SignOffPhaseProperties"],
+        expected_triggered_issues=[
+            "The Sign-Off Phase SHOULD have an agent"
+        ],
+        profile_identifier="five-safes-crate",
+        rocrate_entity_mod_sparql=sparql,
+    )
