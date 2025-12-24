5b358798f716b3fd8e2747f89e09bf68281731f5
EttoreM <ettore.murabito@manchester.ac.uk>
2025-12-24 14:03:43 +0000

    Added rules about startTime and endTime

--- tests/integration/profiles/five-safes-crate/test_5src_13_validation_phase.py
+++ tests/integration/profiles/five-safes-crate/test_5src_13_validation_phase.py
@@ -100,8 +100,74 @@ def test_5src_validation_check_has_action_status_with_not_allowed_value():
         expected_triggered_requirements=["ValidationCheck"],
         expected_triggered_issues=[
             (
-                "The value of actionStatus MUST be one of the allowed values: "
-                "PotentialActionStatus; ActiveActionStatus; CompletedActionStatus; FailedActionStatus."
+                "The value of actionStatus MUST be one of the allowed "
+                "values: PotentialActionStatus; ActiveActionStatus; CompletedActionStatus; FailedActionStatus."
+            )
+        ],
+        profile_identifier="five-safes-crate",
+        rocrate_entity_mod_sparql=sparql,
+    )
+
+
+def test_5src_validation_check_start_time_not_iso_standard():
+    sparql = (
+        SPARQL_PREFIXES
+        + """
+        DELETE {
+            ?c schema:startTime ?t .
+        }
+        INSERT {
+            ?c schema:startTime "1st of Jan 2021" .
+        }
+        WHERE {
+            ?c schema:additionalType shp:ValidationCheck ;
+               schema:startTime ?t .
+        }
+        """
+    )
+
+    do_entity_test(
+        rocrate_path=ValidROC().five_safes_crate_result,
+        requirement_severity=Severity.REQUIRED,
+        expected_validation_result=False,
+        expected_triggered_requirements=["ValidationCheck"],
+        expected_triggered_issues=[
+            (
+                "ValidationCheck --> `startTime` MUST follows the RFC 3339 standard "
+                "(YYYY-MM-DD'T'hh:mm:ss[.fraction](Z | ±hh:mm))."
+            )
+        ],
+        profile_identifier="five-safes-crate",
+        rocrate_entity_mod_sparql=sparql,
+    )
+
+
+def test_5src_validation_check_end_time_not_iso_standard():
+    sparql = (
+        SPARQL_PREFIXES
+        + """
+        DELETE {
+            ?c schema:endTime ?t .
+        }
+        INSERT {
+            ?c schema:endTime "1st of Jan 2021" .
+        }
+        WHERE {
+            ?c schema:additionalType shp:ValidationCheck ;
+               schema:endTime ?t .
+        }
+        """
+    )
+
+    do_entity_test(
+        rocrate_path=ValidROC().five_safes_crate_result,
+        requirement_severity=Severity.REQUIRED,
+        expected_validation_result=False,
+        expected_triggered_requirements=["ValidationCheck"],
+        expected_triggered_issues=[
+            (
+                "ValidationCheck --> `endTime` MUST follows the RFC 3339 standard "
+                "(YYYY-MM-DD'T'hh:mm:ss[.fraction](Z | ±hh:mm))."
             )
         ],
         profile_identifier="five-safes-crate",
@@ -197,6 +263,36 @@ def test_5src_Validation_check_does_not_have_action_status_property():
         expected_validation_result=False,
         expected_triggered_requirements=["ValidationCheck"],
         expected_triggered_issues=["ValidationCheck SHOULD have actionStatus property."],
+        profile_identifier="five-safes-crate",
+        rocrate_entity_mod_sparql=sparql,
+    )
+
+
+def test_5src_download_action_does_not_have_end_time():
+    sparql = (
+        SPARQL_PREFIXES
+        + """
+        DELETE {
+            ?s schema:endTime ?time .
+        }
+        WHERE {
+            ?s schema:additionalType shp:ValidationCheck ;
+               schema:endTime ?time .
+        }
+        """
+    )
+
+    do_entity_test(
+        rocrate_path=ValidROC().five_safes_crate_result,
+        requirement_severity=Severity.RECOMMENDED,
+        expected_validation_result=False,
+        expected_triggered_requirements=["ValidationCheck"],
+        expected_triggered_issues=[
+            (
+                "ValidationCheck SHOULD have the `endTime` property if `actionStatus` "
+                "is either CompletedActionStatus or FailedActionStatus."
+            )
+        ],
         profile_identifier="five-safes-crate",
         rocrate_entity_mod_sparql=sparql,
     )
