a0e72adadae4b8e4d06982ba52a007c92be8b5e8
Eli Chadwick <eli.chadwick@manchester.ac.uk>
2026-01-05 13:35:56 +0000

    allow do_entity_test to accept arbitrary settings

--- tests/shared.py
+++ tests/shared.py
@@ -110,9 +110,12 @@ def do_entity_test(
     rocrate_relative_root_path: Optional[str] = None,
     metadata_only: bool = False,
     metadata_dict: Optional[dict] = None,
+    **kwargs,
 ):
     """
-    Shared function to test a RO-Crate entity
+    Shared function to test a RO-Crate entity.
+
+    Additional keyword arguments (kwargs) are passed along to initialise ValidationSettings.
     """
     assert not (
         rocrate_entity_patch and rocrate_entity_mod_sparql
@@ -186,7 +189,8 @@ def do_entity_test(
                     "rocrate_relative_root_path": rocrate_relative_root_path,
                     "metadata_only": metadata_only,
                     "metadata_dict": metadata_dict,
-                }
+                },
+                **kwargs,
             )
         )
         logger.debug("Expected validation result: %s", expected_validation_result)
