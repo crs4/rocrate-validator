168fac23e6f2592669497085bde6e9de9d765991
Douglas Lowe <10961945+douglowe@users.noreply.github.com>
2026-02-23 15:07:44 +0000

    5S crate integration test, rename inherited profiles flag

--- tests/integration/profiles/five-safes-crate/test_valid_5src.py
+++ tests/integration/profiles/five-safes-crate/test_valid_5src.py
@@ -70,7 +70,7 @@ def test_valid_five_safes_crate_request_recommended():
             SKIP_LOCAL_DATA_ENTITY_EXISTENCE_CHECK_IDENTIFIER,
             SKIP_WEB_RESOURCE_AVAILABILITY_IDENTIFIER,
         ],
-        disable_inherited_profiles_reporting=True,
+        disable_inherited_profiles_issue_reporting=True,
     )
 
 
