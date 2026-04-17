1041dbb6566042420f21ec29073344980b19ec49
Marco Enrico Piras <kikkomep@crs4.it>
2026-04-17 16:21:43 +0200

    test(ro-crate-1.2): :wrench: add test data folder index

--- tests/integration/profiles/ro-crate-1.2/test_detached_rocrates.py
+++ tests/integration/profiles/ro-crate-1.2/test_detached_rocrates.py
@@ -15,7 +15,7 @@
 import logging
 
 from rocrate_validator import models
-from tests.ro_crates_1_2 import AttachedROCrates, DetachedROCrates, MetadataDocument, MetadataDocumentFormat
+from tests.ro_crates_v1_2 import AttachedROCrates, DetachedROCrates, MetadataDocument, MetadataDocumentFormat
 from tests.shared import do_entity_test
 
 logger = logging.getLogger(__name__)
